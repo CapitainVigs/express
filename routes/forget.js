8aedc7d4658b341235fe07191bd17a5811309943
CapitainVigs <vitoireofgod@gmail.com>
2020-10-06 11:31:36 +0100

    Update forget.js
    up

--- routes/forget.js
+++ routes/forget.js
@@ -76,7 +76,7 @@ forgetRouter.route('/reset-password')
 
                         let mailSent = transporter.sendMail(mailOptions)// envoi de courrier à l'utilisateur où il peut réinitialiser le mot de passe.L'identifiant de l'utilisateur et le jeton généré sont envoyés en tant que paramètres dans un lien
                         if (mailSent) {
-                            return res.json({ success: true, message: 'Vérifiez votre messagerie pour réinitialiser votre mot de passe.' })
+                            return res.json({  success: true, message: 'Vérifiez votre messagerie pour réinitialiser votre mot de passe.' })
                         } else {
                             return throwFailed(error, 'Incapable denvoyer des emails.');
                         }
@@ -87,56 +87,57 @@ forgetRouter.route('/reset-password')
 
     })
 
-    forgetRouter.post('/verify-token', (req, res) => {
-        const token = req.body.resetPasswordToken
-        
-        ResetPassword.findOne({ resetPasswordToken: token })
-            .then( (resetPassword)  => {
-               
-                if (!resetPassword) {
-                    return throwFailed(res, 'Token invalide.')
-                } 
-        
-                if (resetPassword ) {
-                    return res.status(200).send({
-                        message: "Token valid !" ,
-                        bon: 1
-                    });                  
-                } 
-            }, (err) => next(err))
-    });
+forgetRouter.post('/verify-token', (req, res) => {
+    const token = req.body.resetPasswordToken
+
+    ResetPassword.findOne({ resetPasswordToken: token })
+        .then((resetPassword) => {
+
+            if (!resetPassword) {
+                return throwFailed(res, 'Token invalide.')
+            }
+
+            if (resetPassword) {
+                return res.status(200).send({
+                    message: "Token valid !",
+                    bon: 1,
+                    userId: resetPassword.userId
+                });
+            }
+        }, (err) => next(err))
+});
 
 
-    forgetRouter.post('/nouveau-password', (req, res) => {
-    
-    
-    ResetPassword.findOne({ where: { userId: userId, status: 0 } })
-        .then(function (resetPassword) {
+forgetRouter.post('/nouveau-password', (req, res) => {
+        const userId = req.body.userId
+
+    ResetPassword.findOne( { userId: userId, status: 0 } )
+        .then( (resetPassword) => {
             if (!resetPassword) {
                 return throwFailed(res, 'Invalid or expired reset token.')
             }
             const userId = resetPassword.userId
-               
-                
-                    User.update({
-                        password: bcrypt.hashSync(req.body.password, 8),
-                    },
-                        { where: { _id: userId } }
-                    ).
-                        then(() => {
-                            ResetPassword.update({
-                                status: 1
-                            }, { where: { _id: resetPassword._id } }).
-                                then((msg) => {
-                                    if (!msg)
-                                        throw err
-                                    else
-                                        res.json({ success: true, message: 'Password Updated successfully.' })
-                                })
-                        
-               
-            });
-        }).catch(error => throwFailed(error, ''))
+
+
+            User.update({
+                password: bcrypt.hashSync(req.body.password, 8),
+            },
+                { where: { _id: userId } }
+            ).
+                then(() => {
+                    ResetPassword.update({
+                        status: 1
+                    }, { where: { _id: resetPassword._id } }).
+                        then((msg) => {
+                            if (!msg)
+                                throw err
+                            else
+                                res.json({ success: true, message: 'Password Updated successfully.' })
+                        }, (err) => next(err))
+
+
+                }, (err) => next(err));
+        }, (err) => next(err))
 })
 
 
