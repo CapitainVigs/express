449b7e944006d7da999a52e51799d7deea768d8f
capitaine <vitoireofgod@gmail.com>
2020-12-10 14:41:02 +0100

    Update upload.js
    done

--- routes/upload.js
+++ routes/upload.js
@@ -14,12 +14,15 @@ const MIME_TYPES = {
 };
 
 uploadRouter.post('/:id_user', (req, res) => {
-	fs.writeFile('./images/out.png', req.body.imgsource, 'base64', (err) => {
+    const filename='./images/'+req.params.id_users+Date.now()+'.png'
+	fs.writeFile(filename, req.body.imgsource, 'base64', (err) => {
 		if (err) console.log(err.message)
-	})
+	}).then(
+        users.findByIdAndUpdate(req.params.id_users,{ imageUrl: filename})
+    )
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json("ok");
+    res.json({success: true});
 })
 
 /*
